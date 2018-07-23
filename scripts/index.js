ce3144279d886d402f2dfc6832221b0bd1f26896
Dylan Schroeder <schrodac@gmail.com>
2018-07-23 17:43:40 -0500

    show only for template done

--- scripts/index.js
+++ scripts/index.js
@@ -14,6 +14,21 @@ var app = app || {};
     apiUrl: module.isProduction ? productionApiUrl : developmentApiUrl
   };
 
+  module.showOnly = (selector) => {
+    $('.container').hide();
+    $(selector).show();
+  };
+  
+  const templateCache = {};
+  module.render = (templateId, dataToRender) => {
+    let template = templateCache[templateId];
+
+    if(!template) {
+      template = Handlebars.compile(document.getElementById(templateId).innerText);
+      templateCache[templateId] = template;
+    }
+    return template(dataToRender);
+  };
 
 
 
