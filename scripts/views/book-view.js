88705886cdbd0790b2518ecaea6de84ce9449176
Dylan Schroeder <schrodac@gmail.com>
2018-07-25 15:37:05 -0500

    input form done

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -7,13 +7,26 @@ var app = app || {};
 
   bookView.initIndexPage = () => {
     app.showOnly('.book-view');
+
+    $('#book-list').empty();
     app.Book.all.forEach(book => $('#book-list').append(book.toHtml()));
   };
+
+  bookView.initDetailPage = book => {
+    app.showOnly('.book-detail');
+
+    $('#book-detail').empty().append(book.toHtml());
+  };
+
+  bookView.initAddPage = () => {
+    console.log('initAddPage');
+    app.showOnly('.form-view');
+  };
+
+  $('#add-form').on('submit', function (event){
+    event.preventDefault();
+  });
 
   module.bookView = bookView;
 
 })(app);
-
-$(function() {
-  app.Book.fetchAll(app.bookView.initIndexPage);
-});
